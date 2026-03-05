2be3e88c619f580889f63c29949ceeb55ed86d62
Levalicious <lev@levk.dev>
2026-03-05 16:29:37 -0500

    Tests

--- levalicious_mcp_server_fetch/json.py
+++
@@ -1,3 +0,0 @@
-JsonType = None | bool | int | float | str | dict[str, "JsonType"] | list["JsonType"]
-
-StrictJsonType = bool | int | float | str | dict[str, "StrictJsonType"] | list["StrictJsonType"]
