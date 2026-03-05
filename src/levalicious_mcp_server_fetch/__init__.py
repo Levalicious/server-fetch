ab6d602d1da902e01edfc4c97d9b3003483e96d1
Levalicious <lev@levk.dev>
2026-03-05 14:48:32 -0500

    Use `tap` instead of `argparse`

--- src/levalicious_mcp_server_fetch/__init__.py
+++ src/levalicious_mcp_server_fetch/__init__.py
@@ -1,23 +1,16 @@
 from .server import serve
+from tap import Tap
 
+class FetchRequestParser(Tap):
+    user_agent: str # pyright: ignore[reportUninitializedInstanceVariable]
+    ignore_robots_txt: bool = False
+    proxy_url: str  # pyright: ignore[reportUninitializedInstanceVariable]
 
 def main():
     """MCP Fetch Server - HTTP fetching functionality for MCP"""
-    import argparse
     import asyncio
-
-    parser = argparse.ArgumentParser(
-        description="give a model the ability to make web requests"
-    )
-    parser.add_argument("--user-agent", type=str, help="Custom User-Agent string")
-    parser.add_argument(
-        "--ignore-robots-txt",
-        action="store_true",
-        help="Ignore robots.txt restrictions",
-    )
-    parser.add_argument("--proxy-url", type=str, help="Proxy URL to use for requests")
-
-    args = parser.parse_args()
+    
+    args = FetchRequestParser(underscores_to_dashes=True).parse_args()
     asyncio.run(serve(args.user_agent, args.ignore_robots_txt, args.proxy_url))
 
 
